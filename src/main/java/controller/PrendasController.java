53910da8acd845e3dfb5bc8fa6f0ef12deeb2654
Dolores Lopez Joffre <dolores.lopezjoffre@gmail.com>
2023-10-06 21:50:37 -0300

    Change desc

---
+++ src/main/java/controller/PrendasController.java
@@ -0,0 +1,22 @@
+package controller;
+
+import io.github.flbulgarelli.jpa.extras.simple.WithSimplePersistenceUnit;
+import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.Map;
+import model.RepositorioGuardarropas;
+import spark.ModelAndView;
+import spark.Request;
+import spark.Response;
+
+public class PrendasController implements WithSimplePersistenceUnit {
+
+  public ModelAndView obtener(Request request, Response response) {
+    var id = Long.parseLong(request.params("id"));
+
+    Map<String, Object> modelo = new HashMap<>();
+    modelo.put("anio", LocalDate.now().getYear());
+    modelo.put("guardarropas", RepositorioGuardarropas.instance().obtener(id));
+    return new ModelAndView(modelo, "prendas/detalle.html.hbs");
+  }
+}
